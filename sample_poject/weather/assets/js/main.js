a2251c04f9cdcbb5a178ee2ee8e9faf648093924
Abhijit Gayen <76001287+zero26-hack-bit@users.noreply.github.com>
2021-06-02 00:15:50 +0530

    need to change

--- sample_poject/weather/assets/js/main.js
+++ sample_poject/weather/assets/js/main.js
@@ -10,7 +10,7 @@ $(".search").on("change keyup paste", function() {
   $(".is-custom-color-icon-check").css('display', 'none');
   let = SEARCH_TERM = $(".search").val();
   let UNITS = "metric" // TODO: Check what the user selected and pass it to the url
-  const API_KEY = '' // Open Weather Map API KEY
+  const API_KEY = 'e24552fa091342f7f4e86823c3f8ddce'
   const SEARCH_URL = `https://api.openweathermap.org/data/2.5/weather?q=${SEARCH_TERM}&units=${UNITS}&appid=${API_KEY}`
 
   if (!SEARCH_TERM) {
@@ -27,7 +27,6 @@ $(".search").on("change keyup paste", function() {
       document.getElementById("location_temp").innerHTML = res['main']['temp'];
       document.getElementById("location_temp_max").innerHTML = res['main']['temp_max'];
       document.getElementById("location_temp_min").innerHTML = res['main']['temp_min'];
-      document.getElementById("location_weather_description").innerHTML = res['weather']['description'];
       $(".is-custom-hero-content p").removeClass("is-loading");
       $(".is-custom-color-icon-check").css('display', 'flex');
     } else {
