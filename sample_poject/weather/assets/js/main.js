7a9da43e30e1a189bfb9b908d978242df6fa8d36
Abhijit Gayen <76001287+zero26-hack-bit@users.noreply.github.com>
2021-06-02 23:32:50 +0530

    .

--- sample_poject/weather/assets/js/main.js
+++ sample_poject/weather/assets/js/main.js
@@ -1,65 +1,50 @@
 $(document).ready(function() {
   $(".search").val("");
   $(".search").focus();
-  $(".is-custom-color-icon-check").css('display', 'none');
-  $(".is-custom-color-icon-ban").css('display', 'none');
-  $(".is-custom-notification").css('display', 'none');
+  $(".getapi").css('display', 'none');
+  // $(".nogetapi").css('display', 'none');
+  // $(".is-custom-notification").css('display', 'none');
+  
 });
 
 $(".search").on("change keyup paste", function() {
-  $(".is-custom-color-icon-check").css('display', 'none');
+   $(".getapi").css('display', 'none');
   let = SEARCH_TERM = $(".search").val();
   let UNITS = "metric" // TODO: Check what the user selected and pass it to the url
   const API_KEY = 'e24552fa091342f7f4e86823c3f8ddce'
   const SEARCH_URL = `https://api.openweathermap.org/data/2.5/weather?q=${SEARCH_TERM}&units=${UNITS}&appid=${API_KEY}`
-
-  if (!SEARCH_TERM) {
-    $(".is-custom-hero-content p").removeClass("is-loading");
+  $(".nogetapi").css('display', 'flex');
+  function time( sec,lpc){
+    var date = new Date(sec * 1000);
+    var timestr = date.toLocaleTimeString();
+    console.log(date, timestr);
+    return timestr
+// $("#d1").html(date);
+// $("#d2").html(timestr);
   }
-
-  $(".is-custom-hero-content p").removeClass("is-loading");
-  $(".is-custom-color-icon-ban").css('display', 'flex');
   $.get(SEARCH_URL, function(res) {
     if (res) {
-      $(".is-custom-color-icon-ban").css('display', 'none');
-      $(".is-custom-hero-content p").addClass("is-loading");
+      $(".nogetapi").css('display', 'none');
       document.getElementById("location_name").innerHTML = res['name'] + ", " + res['sys']['country'];
+      document.getElementById("location_lon").innerHTML = res.coord.lon;
+      document.getElementById("location_lat").innerHTML = res.coord.lat;
+      document.getElementById("location_temp_syssunrise").innerHTML =time( res.sys.sunrise);
+      document.getElementById("location_temp_syssunset").innerHTML = time(res.sys.sunset);
       document.getElementById("location_temp").innerHTML = res['main']['temp'];
       document.getElementById("location_temp_max").innerHTML = res['main']['temp_max'];
       document.getElementById("location_temp_min").innerHTML = res['main']['temp_min'];
-      $(".is-custom-hero-content p").removeClass("is-loading");
-      $(".is-custom-color-icon-check").css('display', 'flex');
+      document.getElementById("location_temp_pressure").innerHTML = res.main.pressure;
+      document.getElementById("location_temp_humidity").innerHTML = res.main.humidity;
+
+      document.getElementById("location_temp_windspeed").innerHTML = res.wind.speed;
+      document.getElementById("location_temp_windgust").innerHTML = res.wind.gust;
+      document.getElementById("location_temp_winddeg").innerHTML = res.wind.deg;
+      document.getElementById("location_temp_cloudsall").innerHTML = res.clouds.all;
+       $(".getapi").css('display', 'flex');
     } else {
-      $(".is-custom-hero-content p").addClass("is-loading");
-      $(".is-custom-color-icon-ban").css('display', 'flex');
+      alert("not ok")
+      // $(".notgetapi").css('display', 'flex');
+      
     }
   });
-});
-
-
-// Bulma Menu Toggler
-document.addEventListener('DOMContentLoaded', function() {
-
-  // Get all "navbar-burger" elements
-  var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-
-  // Check if there are any navbar burgers
-  if ($navbarBurgers.length > 0) {
-
-    // Add a click event on each of them
-    $navbarBurgers.forEach(function($el) {
-      $el.addEventListener('click', function() {
-
-        // Get the target from the "data-target" attribute
-        var target = $el.dataset.target;
-        var $target = document.getElementById(target);
-
-        // Toggle the class on both the "navbar-burger" and the "navbar-menu"
-        $el.classList.toggle('is-active');
-        $target.classList.toggle('is-active');
-
-      });
-    });
-  }
-
 });
